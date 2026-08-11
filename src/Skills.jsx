aa2f07c76ea9a774a677ef0985c96a48c36b15d5
sinshr <98868456+sinnshr@users.noreply.github.com>
2026-08-11 21:39:30 +0330

    feat: update Skills component layout and styling; adjust CircularGallery properties for better visual consistency

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -2,19 +2,17 @@ import CircularGallery from './animations/CircularGallery';
 
 const Skills = () => {
     return (
-        <div name="skills" className="relative mx-auto w-full h-svh max-w-6xl rounded-[2rem] px-5 py-10 sm:px-10 flex-col justify-center items-center">
-            <div className="mx-auto flex w-full max-w-3xl flex-col items-center justify-center text-center mt-[10%]">
-                <h2 className="mt-6 text-4xl sm:text-3xl inline-flex rounded-full bg-[#5732F0]/25 px-4 py-2 mb-5 font-semibold uppercase tracking-[0.3em] text-indigo-300">
-                    Technologies I Work With
-                </h2>
-
+        <div name="skills" className="relative mx-auto w-full h-svh max-w-6xl rounded-[2rem] px-5 pb-10 pt-[8rem] sm:px-10 flex-col justify-center items-center">
+            <div className="mx-auto mb-10 max-w-2xl text-center">
+                <span className="section-label">Skills</span>
+                <h2 className="section-title mt-5">Div-ing Into My Stack.</h2>
             </div>
             {/* Desktop Circular Gallery */}
             <div className="relative z-10 hidden sm:block">
                 <CircularGallery
                     height={170}
                     bend={0}
-                    textColor="#ffffff"
+                    textColor="#d1d5db"
                     borderRadius={0.05}
                     fontSize="600px"
                     scrollEase={0.05}
@@ -24,13 +22,13 @@ const Skills = () => {
             {/* Mobile Circular Gallery */}
             <div className="relative z-10 mt-[-2rem] block sm:hidden text-2xl">
                 <CircularGallery
-                    height={320}
+                    height={150}zz
                     bend={0}
-                    textColor="#ffffff"
+                    textColor="#d1d5db"
                     borderRadius={0.08}
                     fontSize="750px"
                     scrollEase={1}
-                    scrollSpeed={2}
+                    scrollSpeed={0.4}
                 />
             </div>
         </div>
